285296156474430701c80c9b99b3cfba8a84d6e0
pk5ls20 <pk5ls20@outlook.com>
2024-05-27 17:36:13 +0800

    fix: pprof addr

--- uploadClient/flag.go
+++ uploadClient/flag.go
@@ -7,7 +7,7 @@ import (
 var (
 	runTime     = flag.Int("runtime", 0, "Run time in seconds, set 0 to disable")
 	pprofEnable = flag.Bool("pprof", false, "Enable pprof")
-	pprofAddr   = flag.String("pprofAddr", "http://127.0.0.1:6060", "pprof address")
+	pprofAddr   = flag.String("pprofAddr", "127.0.0.1:6060", "pprof address")
 )
 
 func parseFlags() {
--- uploadClient/pprof/pprof.go
+++ uploadClient/pprof/pprof.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 	"net/http/pprof"
 	"os"
-	"time"
 )
 
 func RunPprof(addr string) {
@@ -17,12 +16,10 @@ func RunPprof(addr string) {
 	mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
 	mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
 	server := http.Server{Addr: addr, Handler: mux}
-	logrus.Infof("pprof debug server start!: %v/debug/pprof", addr)
+	logrus.Infof("pprof debug server start!: http://%v/debug/pprof", addr)
 	if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
 		logrus.Error(err)
 		logrus.Errorf("Failed to start pprof server, please check if the port %s is occupied.", addr)
-		logrus.Error("will exit in 5 seconds...")
-		time.Sleep(time.Second * 5)
 		os.Exit(1)
 	} else {
 		logrus.Infof("pprof server started at %v/debug/pprof", addr)
